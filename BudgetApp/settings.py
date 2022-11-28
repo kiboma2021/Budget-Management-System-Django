8429d9e68e8bd0ba80d0239f010ebe1735c1ef30
kiboma <omayioben@gmail.com>
2022-11-28 16:50:50 +0300

    Update settings file

--- BudgetApp/settings.py
+++ BudgetApp/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR/ 'staticfiles' #Used in a remote server when we collect static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') #Used in a remote server when we collect static files
+STATICFILES_DIRS = (BASE_DIR/ 'static',)
 
 
 # Default primary key field type
